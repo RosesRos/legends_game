ae58c1e8c070a8123c928bc82c196ce4105e18b1
facesar <rostranespinoza@gmail.com>
2022-08-17 22:02:51 +0300

    files_project

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <h3>hello cucu</h3>
+  <div class="card">
+    <h3 class="card_title">{{name}}</h3>
+    <button class="card_btn" v-on:click="changeName">change name here</button>
   </div>
 </template>
 
@@ -8,9 +9,41 @@
 
 export default {
   name: 'App',
+  data() {
+    return {
+      name: 'luis',
+    }
+  },
+  methods: {
+    changeName: function() {
+      this.name = 'carlos'
+    }
+  }
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
 
 <style>
+  .card {
+    text-align: center;
+    max-width: 40rem;
+    width: 100%;
+    background: rgba(9, 9, 137, 0.2);
+    margin: 0 auto;
+    padding: 1rem;
+  }
 
+  .card_title {
+    color: brown;
+    font-size: 1.6rem;
+  }
+
+  .card_btn {
+    padding: 1rem;
+    background-color: rgba(137, 9, 107, 0.2);
+    color: wheat;
+    border: none;
+  }
 </style>
