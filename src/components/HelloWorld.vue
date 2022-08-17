358274bfedecdfee26b13ff8631f79f2d7392a1c
facesar <rostranespinoza@gmail.com>
2022-08-17 20:52:07 +0300

    files_project

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,11 +14,11 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
 h1 {
   margin: 40px 0 0;
   color: gold;
-  font-size: 4rem;
+  font-size: 0.2rem;
 }
 
 </style>
