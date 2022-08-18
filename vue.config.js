44977ac3d054b1eb4b3f3c8eed670e94a5585f94
facesar <rostranespinoza@gmail.com>
2022-08-18 13:52:28 +0300

    files_project

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production' ? '/' : '/legends_game/',
   outputDir: process.env.NODE_ENV === 'production' ? __dirname+'/dist' : __dirname+'/dist',
-  css: {
-    extract: true,
-  } 
+  // css: {
+  //   extract: true,
+  // } 
 })
