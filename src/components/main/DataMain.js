cd9bd4769058b4e439c90a7483dbac3f2815446a
facesar <rostranespinoza@gmail.com>
2022-08-22 22:00:01 +0300

    files_project

--- src/components/main/DataMain.js
+++ src/components/main/DataMain.js
@@ -21,25 +21,25 @@ export const Icons = [
     {
         id: 1,
         description: 'Select Your Details',
-        // img: require('@/assets/img/list.svg'),
+        img: require('../../assets/img/list.svg'),
         alt: 'img list'
     },
     {
         id: 2,
         description: 'Check out as a Guest',
-        // img: require('@/assets/img/avat.svg'),
+        img: require('../../assets/img/avat.svg'),
         alt: 'img avatar'
     },
     {
         id: 3,
         description: 'Select a Payment Apliers',
-        // img: require('@/assets/img/wallet.svg'),
+        img: require('../../assets/img/wallet.svg'),
         alt: 'img wallet'
     },
     {
         id: 4,
         description: 'Track Your Road',
-        // img: require('@/assets/img/wayroad.svg'),
+        img: require('../../assets/img/wayroad.svg'),
         alt: 'img road'
     },
 ];
